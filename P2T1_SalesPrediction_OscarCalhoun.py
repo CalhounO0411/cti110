7f04e40255faf152385263c6c1544b65ceccda24
CalhounO0411 <56103127+CalhounO0411@users.noreply.github.com>
2019-10-03 00:48:29 -0400

    Add files via upload
    First submission

---
+++ P2T1_SalesPrediction_OscarCalhoun.py
@@ -0,0 +1,16 @@
+# This program will calulate the total sales projected
+total_sales = float(input("Enter the projected sales:"))
+
+# 10/2/2019
+
+# CTI-110 P2T1 - Sales Prediction
+
+# Oscar Calhoun
+
+# Calculate the profit as 23 percent of total sales
+profit = total_sales * 0.23
+
+# Display the profit
+print("The profit is $", format(profit,',.2f'))
+
+
